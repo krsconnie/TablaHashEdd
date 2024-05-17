dd8534d49ece14e38ffd9677ded071157b17190f
Constanza Cristinich <109244120+krsconnie@users.noreply.github.com>
2024-05-17 02:09:10 -0400

    Se agrega la función para cargar el dataset a la hashtable en el caso que key = Nombre de usuario

---
+++ loadCSV_UN.h
@@ -0,0 +1,47 @@
+#ifndef LOADCSV_UN_H
+#define LOADCSV_UN_H
+
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include "twitterUsers.h"
+
+
+//Carga de archivos solo para probar la inserción de datos a la hash table
+//tomando key = userName
+
+template <typename HashTable> //Cualquiera de las posibles tablas
+
+void loadCSV_UserName(const std::string& filename, HashTable& hashtable){
+	
+	std::ifstream file(filename);
+	std::string line;
+
+	if(file.is_open()){
+
+		std::getline(file, line);
+
+		while (std::getline(file, line)){
+			
+			std::stringstream ss(line);
+			std::string uni, userID, userName, numberTweets, friends, followers, since;
+
+			std::getline(ss, uni, ',');
+			std::getline(ss, userID, ',');
+			std::getline(ss, userName, ',');
+			std::getline(ss, numberTweets, ',');
+			std::getline(ss, friends, ',');
+			std::getline(ss, followers, ',');
+			std::getline(ss, since, ',');
+
+			twitterUser user = {uni, std::stoll(userID), userName, std::stoi(numberTweets), std::stoi(friends), std::stoi(followers), since};
+			hashTable.insert(userName, twitterUser);
+
+
+		}
+
+		file.close();
+	}
+}
+#endif 
