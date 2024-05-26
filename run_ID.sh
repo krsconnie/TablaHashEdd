a40fc7684218950d6983e9aae4d0792ed8befa50
Diego-ipt <106196745+Diego-ipt@users.noreply.github.com>
2024-05-25 23:27:16 -0400

    se arreglo un error en la cantidad de datos ingresados en run_ID.sh

--- run_ID.sh
+++ run_ID.sh
@@ -6,8 +6,8 @@ echo "ejecutable;numElementos;tiempo"
 # Repeat experiments 30 times
 for (( c=1; c<=30; c++ ))
 do
-    # Execute test_ID with different input sizes
-    for n in 100000 200000 300000 400000 500000
+    # Execute test_ID with different input sizes, max 21070
+    for n in 5000 10000 15000 20000 21070
     do
         ./test_ID $n >> experiment_results_ID.log  # Redirect output to a log file
     done
